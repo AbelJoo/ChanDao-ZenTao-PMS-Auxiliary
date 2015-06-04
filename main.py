b1b6fc6e5c26019dc7ff3f7e60f02ad9934518df
Abel Joo <abel.joo.1994@gmail.com>
2015-06-04 12:17:16 +0800

    完成配置文件读取

--- main.py
+++ main.py
@@ -6,9 +6,27 @@ from bean import BugBean
 import zen_tao_client
 
 
+def scanner_config():
+    """
+    读取配置文件
+    :return:
+    """
+    with open("config.inf", "r") as f:
+        lines = f.readlines()
+    for line in lines:
+        if line.startswith("#"):
+            lines.remove(line)
+    kv_map = {}
+    for line in lines:
+        kv = line.strip().split(":", 1)
+        kv_map[kv[0]] = kv[1]
+    return kv_map
+
 if __name__ == '__main__':
-    zen_tao_client.ACCOUNT = "zjj"
-    zen_tao_client.PASSWORD = "123456"
+    kv_map = scanner_config()
+    zen_tao_client.ACCOUNT = kv_map["account"]
+    zen_tao_client.PASSWORD = kv_map["password"]
+    print(kv_map)
 
     while True:
         bug_bean = zen_tao_client.get_my_bug()
@@ -23,6 +41,9 @@ if __name__ == '__main__':
                 msg_content += "级别：" + bean.level + "\n"\
                                + "类型：" + bean.error_type + "\n"\
                                + "From:" + bean.author
+        else:
+            msg_title = "当前共有" + str(len(bug_bean)) + "条bug"
+            msg_content = "无"
 
         if msg_title\
                 and msg_content:
